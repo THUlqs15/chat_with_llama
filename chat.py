ae0ecafac1cd67d6d9360512d4ebfc7762dd32d3
THUlqs15 <1475415620@qq.com>
2024-09-08 23:54:50 +0800

    Update chat.py

--- chat.py
+++ chat.py
@@ -4,7 +4,8 @@ from transformers import AutoModelForCausalLM, AutoTokenizer
 # 使用你提供的模型路径
 model_path = "/root/lqs/LLaMA-Factory-main/llama3_models/models/Meta-Llama-3-8B-Instruct1"
 tokenizer = AutoTokenizer.from_pretrained(model_path)
-tokenizer.pad_token = tokenizer.eos_token  # 将 eos_token 用作 pad_token
+tokenizer.pad_token = tokenizer.eos_token
+tokenizer.padding_side = "left"
 
 model = AutoModelForCausalLM.from_pretrained(model_path, torch_dtype=torch.float16)
 model.eval()
