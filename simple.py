848f67b79ee9d5db19bfa54627e02424fe76acbf
THUlqs15 <1475415620@qq.com>
2024-09-12 00:37:06 +0800

    Update simple.py

--- simple.py
+++ simple.py
@@ -1,8 +1,11 @@
 from transformers import LlamaForCausalLM, LlamaTokenizer
 
 model_path = "/root/lqs/LLaMA-Factory-main/llama3_models/models/Meta-Llama-3-8B-Instruct1"
-tokenizer = LlamaTokenizer.from_pretrained(model_path)
-model = LlamaForCausalLM.from_pretrained(model_path)
+tokenizer = AutoTokenizer.from_pretrained(model_path)
+tokenizer.pad_token = tokenizer.eos_token  # 将 eos_token 用作 pad_token
+tokenizer.padding_side = "left"
+model = AutoModelForCausalLM.from_pretrained(model_path, torch_dtype=torch.float16)
+model.eval()
 
 inputs = tokenizer("Hello, how are you?", return_tensors="pt")
 outputs = model.generate(inputs['input_ids'], max_length=50)
