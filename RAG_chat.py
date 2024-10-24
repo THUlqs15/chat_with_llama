28e1375c49ad2589a49fe5f6e166847b80ae93ac
THUlqs15 <1475415620@qq.com>
2024-10-23 21:58:59 -0400

    Update RAG_chat.py

--- RAG_chat.py
+++ RAG_chat.py
@@ -18,17 +18,8 @@ dataset = load_dataset("not-lain/wikipedia",revision = "embedded")
 data = dataset["train"]
 data = data.add_faiss_index("embeddings") # column name that has the embeddings of the dataset
 
-
-
-
-
-
 model_id = "/workspace/LLaMA-Factory/models"
 adapter_name_or_path = "/workspace/LLaMA-Factory/saves_lora/5"
-# use quantization to lower GPU usage
-bnb_config = BitsAndBytesConfig(
-    load_in_4bit=True, bnb_4bit_use_double_quant=True, bnb_4bit_quant_type="nf4", bnb_4bit_compute_dtype=torch.bfloat16
-)
 
 tokenizer = AutoTokenizer.from_pretrained(model_id)
 model = AutoModelForCausalLM.from_pretrained(
